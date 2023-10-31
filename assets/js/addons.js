4b910f6fd61f3e88682cc3a2fbdff46fa4f58d5d
VIN <115197877+Binn12345@users.noreply.github.com>
2023-10-31 12:00:51 +0800

    temp push

--- assets/js/addons.js
+++ assets/js/addons.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+    // static '2000' as mybirthyear
+    // autocal age based on year
     const age = new Date().getFullYear() - 2000;
     $('#ageDisplay').text(age);
 
